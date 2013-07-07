167e9d178eb55ecb90021e9162e9ef5b71897df5
JohnnyPP <johnnnyppp@gmail.com>
2013-07-07 20:12:52 +0200

    Initial release
    http://compositeextensions.codeplex.com/releases/view/26312
    http://compositeextensions.codeplex.com/discussions/53731?ProjectName=compositeextensions

---
+++ App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using PropertyChangedDemo.Domain;
+
+namespace PropertyChangedDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Person1 luke = new Person1();
+            luke.Name = "Luke";
+
+            Person2 lea = new Person2();
+            lea.Name = "Lea";
+
+            Person3 han = new Person3();
+            han.Name = "Han";
+
+            Person4 lando = new Person4();
+            lando.Name = "Lando";
+
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.DataContext = new List<object>() { luke, lea, han, lando };
+            mainWindow.Show();
+        }
+    }
+}
---
+++ Domain/Person.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace PropertyChangedDemo.Domain
+{
+    internal class Person1 : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        private string name;
+        
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+
+    internal class Person2 : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private static readonly PropertyChangedEventArgs NameEventArgs = new PropertyChangedEventArgs("Name");
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged(NameEventArgs);
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, e); }
+        }
+    }
+
+
+    internal class Person3 : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            CheckPropertyName(propertyName);
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        [Conditional("DEBUG")]
+        private void CheckPropertyName(string propertyName)
+        {
+            PropertyDescriptor propertyDescriptor = TypeDescriptor.GetProperties(this)[propertyName];
+            if (propertyDescriptor == null)
+            {
+                throw new InvalidOperationException(string.Format(null,
+                    "The property with the propertyName '{0}' doesn't exist.", propertyName));
+            }
+        }
+    }
+
+
+    internal class Person4 : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private static readonly string NamePropertyName = TypeManager.GetProperty<Person4>(x => x.Name).Name;
+        
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged(NamePropertyName);
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
